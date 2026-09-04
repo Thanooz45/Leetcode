6e13054a109deddc74cd9336a5241cc234e903e0
Thanooz45 <webvinni66@gmail.com>
2026-09-04 12:03:45 +0530

    Time: 6 ms (24.34%), Space: 46.5 MB (92.57%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        int n=lists.length;
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
+
+        for(ListNode node:lists){
+            if(node!=null){
+                pq.add(node);
+            }
+        }
+        ListNode dummy=new ListNode(0);
+        ListNode tail=dummy;
+
+        while(!pq.isEmpty()){
+            ListNode small=pq.poll();
+            tail.next=small;
+            tail=tail.next;
+            if(small.next!=null){
+                pq.add(small.next);
+            }
+        }
+        return dummy.next;
+        
+    }
+}
